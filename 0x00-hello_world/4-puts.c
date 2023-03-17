f8babdc52aead39f743125725076d802a3bb251b
Elinapre <elinaprecious1216@gmai.com>
2023-03-17 13:46:15 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - this is the function
+ *
+ * Return:  always 0
+ */
+
+int main(void)
+{
+         puts("\"Programming is like buiding a multilingual puzzle");
+	 return (0);
+}
+
