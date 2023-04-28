0fdf8780dd0f320b076af3549c5354fd90834ba3
Elinapre <elinaprecious1216@gmai.com>
2023-04-28 14:04:30 -0700

    hello holberton

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,10 +1,9 @@
-format: db `H       global    main
+global    main
           extern    printf
 main:
-	  mov   edi, format
-	  xor   eax, eax
-	  call  printf
-	  mov 	eax, 0
-	  ret
-ello, Holberton\n`,0
-
+          mov   edi, format
+          xor   eax, eax
+          call  printf
+          mov         eax, 0
+          ret
+format: db `Hello, Holberton\n`,0
